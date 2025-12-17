142ed88b60742bf8c3018b0f3f50c01dba9db569
Vazquez Alvarez <jlvazqueza@minsait.com>
2025-12-17 13:13:57 -0600

    Commit inicial

---
+++ go.mod
@@ -0,0 +1,3 @@
+module src/github.com/jlvmexicogit/asatidwh
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import (
+	"src/github.com/jlvmexicogit/asatidwh/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
