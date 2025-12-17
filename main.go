a0cbb89d110a31615599337b48cb9c2f3e14b1be
Vazquez Alvarez <jlvazqueza@minsait.com>
2025-12-17 14:15:28 -0600

    pepe

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"fmt"
+	"runtime"
 	"src/github.com/jlvmexicogit/asatidwh/variables"
 )
 
@@ -8,4 +9,10 @@ func main() {
 	estado,texto := variables.ConviertoaTexto(1223)
 	fmt.Println("estado= ",estado)
 	fmt.Println("estado= ",texto)
+	
+	if os := runtime.GOOS; os=="Linux." || os=="OS X." {
+		fmt.Println("Esto no es windows",os)
+	} else {
+		fmt.Println("Esto si es windows",os)
+	}
 }
